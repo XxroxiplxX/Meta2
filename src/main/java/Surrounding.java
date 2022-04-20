9d5983629fdce0a897d07c60c37d917d50b1977f
polishfighter022 <261696@student.pwr.edu.pl>
2022-04-20 18:16:21 +0200

    pomysly

--- src/main/java/Surrounding.java
+++ src/main/java/Surrounding.java
@@ -1,12 +1,12 @@
 public class Surrounding {
     private Solution[] list_of_perms;
-
-    public Surrounding(int[][] matrix) {
-        this.getN(matrix);
+    //pomysl: trzymajmy permutacje w hashmape wraz z funkcja celu jako klucz, mozemy na biezaco sortowac i miec na wierzchu najlepsze rozwiazanie
+    public Surrounding(int[][] matrix) {    //pytanie w jakiej strukturze chcemy trzymac rozwiazanie. mozemy pozostac przy dwuwymairowej macierzy odleglosci chociaz to skomplikuje funkcje invert
+        this.invert(matrix, 1, 2);  //invert ma odwrocic kolejnosc elementow od i do j, w macierzy 2D musimy zdefuniowac jakis porzadek na elementach
         //na bazie danej permutacji generuj otoczenie typu invert
     }
 
-    public void getN(int[][] matrix) {
+    public void invert(int[][] matrix, int i, int j) {
 
     }
 
