079cd569f3f8a112cb5e863871e3e35410f77e7f
polishfighter022 <261696@student.pwr.edu.pl>
2022-04-20 18:07:23 +0200

    zmiana zarysu projektu

--- src/main/java/Commander.java
+++ src/main/java/Commander.java
@@ -1,3 +1,5 @@
 public class Commander {
     //tutaj uruchamiamy watki
+    //mozemy na kilku watkach odpalic ten sam algorytm startujacy od roznych losowych rozwiazan
+    //niech kazdy watek konczy pracy albo przy osiagnieciu stagnacji (np po 10k probach nie odnalazl lepszego rozwiazania) albo po przekroczeniu sztucznie narzuconej liczby iteracji
 }
